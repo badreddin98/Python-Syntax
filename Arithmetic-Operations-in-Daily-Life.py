32898232873df29b4d2bea9862dc1c8927b9e3e7
badreddin98 <56277339+badreddin98@users.noreply.github.com>
2024-07-04 18:18:36 -0400

    First commit

---
+++ Arithmetic-Operations-in-Daily-Life.py
@@ -0,0 +1,14 @@
+# Task 1: Grocery Store Math:
+# Prices of individual items
+apple = 5
+grape = 7
+banana = 3
+# Calculate the total cost
+totalCost = apple + grape + banana
+print("your total is", totalCost)
+
+# Task 2: Bank Interest:
+savingsAccount = 10000
+interstRate = 0.07
+total = savingsAccount + (interstRate * savingsAccount)
+print("Your total amount in your account after a year is", total)
---
+++ Showcase-Your-Dance-Moves.py
@@ -0,0 +1,15 @@
+# Task 1: Code Correction
+weather = "sunny"
+
+if weather == "sunny":
+  print("Wear sunglasses!")
+else:
+  print("Take an umbrella!")
+
+
+# Task 2: Your Mood Today
+user = input("How you feel today?")
+if user == "happy":
+  print("That's great to hear!")
+elif user =="sad":
+  print("I hope your day gets better!")
